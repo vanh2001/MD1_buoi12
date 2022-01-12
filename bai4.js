8b078269e5415321e4b17075a9333f7a86dc70ba
vanh2001 <anhviet1709@gmail.com>
2022-01-12 16:09:44 +0700

    Add bai 4

---
+++ bai4.js
@@ -0,0 +1,10 @@
+let numbers = [-3, 5, 1, 3, 2, 10];
+let max = numbers[0];
+let index = 0;
+for (let i = 0; i < numbers.length; i++) {
+    if(numbers[i] > max){
+        max = numbers[i];
+        index = i;
+    }
+}
+alert("Max: " + max + " " +"at position" + i);
