c311ef77e0080daa458a61ff59d1ff2a477fcddd
vanh2001 <anhviet1709@gmail.com>
2022-01-12 16:13:12 +0700

    Update bai 4

--- bai4.js
+++ bai4.js
@@ -1,7 +1,8 @@
 let numbers = [-3, 5, 1, 3, 2, 10];
 let max = numbers[0];
 let index = 0;
-for (let i = 0; i < numbers.length; i++) {
+let i;
+for (i = 0; i < numbers.length; i++) {
     if(numbers[i] > max){
         max = numbers[i];
         index = i;
